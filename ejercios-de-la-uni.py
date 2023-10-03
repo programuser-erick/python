1b9e308a41382cbcd74ac4ab006a1bdc692572f1
erick cortijo <108696059+programuser-erick@users.noreply.github.com>
2023-10-03 00:09:29 -0300

    ejercios de la facultad

---
+++ ejercios-de-la-uni.py
@@ -0,0 +1,100 @@
+#Clase del 17 de agosto
+
+""" 1) Ingresar 2 valores siendo los mismos, la base y la altura de un triangulo
+Mostrar la superficie del mismo"""
+
+""" 2) Ingresar valores y calcular su promedio
+Utilizar una sola variable de ingreso de datos"""
+
+#Clase del 24 de agosto
+
+""" 1) Ingresar las 2 notas de los parciales
+Si el promedio entero es menor a 4, informar recursa
+Si el promedio es menor a 7, informar rinde final
+Si el promedio es mayor o igual a 7, informar  aprobado"""
+
+""" 2) Ingresar un año e informar si es un año bisiesto"""
+
+""" 3) Se ingresa por teclado la cantidad, en kilos, de dulce de leche y galletitas y queso crema
+Si cada chocotorta insume: 300gr de dulce, 450 gr de galletitas y 150 gr de queso crema, Cuantas chocotortas se pueden hacer con la cantidad ingresada?"""
+
+""" 4) Se ingresa 3 valores correspondientes a la longitud de los lados de un triangulo 
+Se solicita:
+a- Verificar que los valores ingresados formen un triangulo
+b- Si forman un triangulo, que triangulo es?"""
+
+#Clase del 31 de agosto
+
+""" 1) Mostra los primeros 10 numeros, comenzar desde cero"""
+
+""" 2) Mostra los valores entre 10 y 30 y que sean pares, el contador comienza en cero"""
+
+""" 3) Arrojar 50 veces un dado, Informar cuantas veces salio el 5"""
+
+""" 4) Arrojar la bolilla de ruleta 80 veces, informar cuantas veces salio el valor de la segunda docena (13-24)"""
+
+""" 5) Generar dos numeros aleatorios, el primero entre 1 y 4 : donde 1-oro, 2-basto, 3-espada, 4-copa.
+    El segundo es un numero entre 1 y 12: Informar
+    a- Cuantas veces salio el 7 de espadas
+    b- Cuantas veces salio el AS de basto
+    c- Cuantas veces salio un valor impar
+    Realizar 80 tiradas """
+
+""" 6) Generar valores entre 10 y 1000, y sumarlos. Finalizar el proceso cuando la suma supere los 10000 """
+
+""" 7) Numeros primos """
+
+#Clase del 7 de septiembre
+
+""" 1) Sucesion de fibonacci, generar 20 valores de la sucesion"""
+
+""" 2) Generar 50 valores aleatorios entre 10 y 30
+    Realizar la suma de los valores múltiplos de 4
+    Mostrar el resultado en el Programa principal
+    Determinar si el nro es multiplo de 4 con una funcion"""
+
+""" 3) Generar 50 valores aleatorios entre 10 y 30
+    Realizar la suma de los valores primos
+    Mostrar el resultado en el Programa principal
+    Determinar si el nro es primo con una funcion"""
+
+""" 4) Generar 50 valores aleatorios entre 10 y 30
+    Realizar la suma de los numeros deficientes
+    Mostrar el resultado en el Programa principal
+    Determinar si el nro es deficiente con una funcion"""
+
+""" 5) Generar la sucesion de fibonacci hasta la posicion 30. Mostrar solamente aquellos valores que se encuentren en posiciones impares
+Se recuerda que la sucesion comienza con los valores 1 y 1"""
+
+""" 6) Generar 100 nros aleatorios entre 1 y 100
+Informar cuantos de ellos son nros oblongos"""
+
+#Clase del 14 de septiembre
+
+""" 1) Ingresar un nro u calcular su factorial"""
+
+""" 2) Partiendo de los valores a= 1 y b=1, obtener la sucesion de fibonacci, hasta la posicion 30
+Determinar cuantos valores deficientes hay en la sucesion, a partir de la posicion 4"""
+
+""" 3) Juego:
+    Arrojar un dado obteniendo un valor entre 1 y 6
+    Con un sumador se simulará el avance o el retroceso
+    del jugador. El sumador comienza en cero.
+    El juego finaliza cuando se llegue a 100, y el avance será de la siguiente manera: si sale un nro primo, el sumador avanza ese numero.
+    El 1 y 2, no se considera primo
+    Si sale par, se resta ese numero
+    Si sale el uno, no avanza
+    Por fin de proceso indicar cuantas veces se arrojo el dado"""
+
+""" 4) Realizar 80 vueltas y generar 
+animal= 1-Perro, 2-Gato, 3-Conejo
+sexo= 1-macho, 2-hembra
+Por fin de proceso, informar
+1-Cuantos perros machos fueron atendidos
+2-Cuantos conejos fueron atendidos
+3-Porcentajes de gatos hembras atendidos
+"""
+
+""" 5) Generar 100 valores aleatorios comprendidos entre 20 y 400. De los nros oblongos que surjan, informar cual fue el mayor y el menor valor del lote"""
+
+""" 6) Ingrese una nota válida, entre 1 y 10"""
--- practica.py
+++ practica.py
@@ -1,6 +1,6 @@
 """Mayor o menor: Escribe un programa que solicite al usuario dos números y luego determine cuál de los dos números es mayor."""
 
-#solicitar valores al usuario
+"""#solicitar valores al usuario
 valor1= int(input("porfavor ingrese el primer valor: "))
 valor2= int(input("porfavor ingrese el segundo valor: "))
 
@@ -14,4 +14,47 @@ else:
     elif valor1 == valor2:
         print(f"{valor1} es igual a {valor2}")
     else:
-        print(f"{valor1} es menor a {valor2}")
+        print(f"{valor1} es menor a {valor2}")"""
+
+"Número positivo o negativo: Crea un programa que pida al usuario ingresar un número y luego indique si el número es positivo, negativo o cero."
+
+""" valor= int(input("Ingrese un valor: "))
+
+if valor < 0 :
+    print(f"{valor} es negativo")
+elif valor == 0:
+    print(f"{valor} es igual a cero")
+else:
+    print(f"{valor} es positivo") """
+
+
+""" Calculadora simple: Diseña una calculadora que pueda realizar operaciones de suma, resta, multiplicación y división. El usuario debe seleccionar la operación y luego ingresar dos números. Luego, el programa debe mostrar el resultado de la operación. """
+
+""" print("Calculadora simple")
+print("1. Suma")
+print("2. Resta")
+print("3. Multiplicación")
+print("4. División")
+
+opcion= input("Seleccione una operación (1/2/3/4): ")
+
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+
+suma= num1+num2
+resta= num1-num2
+mult= num1*num2
+div= num1//num2
+
+if opcion == "1":
+    print(suma)
+elif opcion == "2":
+    print(resta)
+elif opcion == "3":
+    print(mult)
+elif opcion == "4":
+    print(div)
+else:
+    print("valores ingresados no válidos") """
+    
+    
